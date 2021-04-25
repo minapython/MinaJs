00d1270a34fde13e48f2a3d0ee4dc90bf0bd642e
minapython <minasourial@protonmail.com>
2021-04-25 22:41:28 +0000

    Output

---
+++ ConsoleLoggingAndOutput.js
@@ -0,0 +1,8 @@
+// this is how to output a string to the console.
+console.log('TEXT GOES HERE')
+// this is how to output a variable to the console
+console.log(VARIABLEHERE)
+// how to display text on a html page
+document.write('TEXT GOES HERE')
+// how to output a variable on a html page
+document.write(VARIABLEHERE)
