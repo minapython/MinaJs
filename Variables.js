fad1b9039bc48d66fef05f7dca4621b6737ba324
minapython <minasourial@protonmail.com>
2021-04-25 19:53:31 +0000

    Variable Declarations

---
+++ Variables.js
@@ -0,0 +1,6 @@
+// These are the different ways to make variables. I am showcasing making variables using let, const, and var.
+let YOURVARIABLENAME = YOURVARIABLEVALUE; // if your value is a string make sure to sorround it in quotes. 
+const YOURVARIABLENAME = YOURVARIABLEVALUE;
+// const cannot be changed. 
+// the let declaration is only in the scope of its code block
+var YOURVARIABLENAME = YOURVARIABLEVALUE;
