bcbfab0329e1243a9c2d693b2774a65b58643def
minapython <minasourial@protonmail.com>
2021-04-25 22:46:59 +0000

    MathOperators

---
+++ MathOperators.js
@@ -0,0 +1,17 @@
+// how to multiply two numbers
+NUMBER1 * NUMBER2
+// how to multiply two variables
+VARIABLE1 * VARIABLE2
+// how to divide two numbers
+NUMBER1 / NUMBER2
+// how to divide two variables
+VARIABLE1 / VARIABLE2
+// how to add two numbers
+NUMBER1 + NUMBER2
+// how to add two variables
+VARIABLE1 + VARIABLE2
+// how to subtract two numbers
+NUMBER1 - NUMBER2
+// how to subtract two variables
+VARIABLE1 - VARIABLE2
+// you can add more numbers in your operations. and you can use more than one at a time.
