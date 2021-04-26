b280845139157389b624e955456333518b1cf0bc
minapython <minasourial@protonmail.com>
2021-04-26 23:53:01 +0000

    Data Types

---
+++ DataTypes.js
@@ -0,0 +1,2 @@
+// in JavaScript there are many different data types, there are strings, which are plain text, numbers, which are just numbers. boolean which is true or false, undefined,BigInt, symbols, arrays, objects, typeof operator, and null.
+// - mina
