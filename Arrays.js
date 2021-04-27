eb8af7f5d2ece0940fcc622ce8513469e42c2375
Maoaksk <jsjs@shsjd.com>
2021-04-27 06:08:34 -0500

    Arrays

---
+++ Arrays.js
@@ -0,0 +1,6 @@
+// arrays are a type of data. here are some examples.
+
+var cars = ["Saab", "Volvo", "BMW"];
+// to create a array, use this sequence.
+var array_name = [item1, item2, ...];       
+// -mina
